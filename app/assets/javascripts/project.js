e7afc2da1b8f16f9e8eaf7a948cab6d9ad6d0582
Jim Lindstrom <jim.lindstrom@gmail.com>
2011-10-09 20:06:57 +0000

    Added tooltip explaining how to get started with tracks on a new project.

--- app/assets/javascripts/project.js
+++ app/assets/javascripts/project.js
@@ -3,4 +3,11 @@
 
 $(document).ready(function() {
   $("img.help").tooltip();
+
+  $(".reverse_tooltip").tooltip({ position: "bottom center", 
+                                  events: { a: "ready" },
+                                  tipClass: "reverse-tooltip" });
+
+  $(".tooltip_default_on").each(function() { $(this).mouseover(); })
 });
+
