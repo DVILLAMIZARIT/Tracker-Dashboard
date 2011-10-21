cee636cda57bbcb007cdbe2a22935ed5c93f9c07
Jim Lindstrom <jim.lindstrom@gmail.com>
2011-10-21 01:27:03 +0000

    Tweaked the last migration.  Trying to make it heroku-friendly.

--- db/migrate/20111020215259_add_tracking_to_users.rb
+++ db/migrate/20111020215259_add_tracking_to_users.rb
@@ -1,6 +1,10 @@
 class AddTrackingToUsers < ActiveRecord::Migration
-  def change
+  def self.up 
     add_column :users, :projects_viewed, :string
     add_column :users, :pageviews, :integer
   end
+  def self.down 
+    remove_column :users, :projects_viewed
+    remove_column :users, :pageviews
+  end 
 end
